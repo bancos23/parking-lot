7f463f8acb72d0a61327f645fdc1bd5ff9b620b5
Gabriel Bancos <bancos.em.gabriel@student.utcluj.ro>
2026-05-08 01:24:05 +0300

    Initial commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+
+
+const message = ref('Loading...')
+
+
+onMounted(async () => {
+	try {
+		const res = await fetch('/api/test')
+		const data = await res.json()
+		message.value = data.message
+	} catch (e) {
+		message.value = 'Error connecting to backend'
+		console.error(e)
+	}
+})
+</script>
+
+<template>
+	<div style="padding: 2rem;">
+	<h1>Parking Admin (Vue + FastAPI)</h1>
+	<p>Backend says: <strong>{{ message }}</strong></p>
+	</div>
+</template>
